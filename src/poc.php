5e06a8ffbfda9f80ccc880c019dc2789c9448b32
Toon Verwerft <toonverwerft@gmail.com>
2020-07-17 08:46:30 +0200

    test

--- src/poc.php
+++ src/poc.php
@@ -12,5 +12,43 @@ function x($data): array {
 x(['string' => 'string']);
 
 
+// ==================================================================================================================
+
 /** @psalm-assert string $x  */
 $x = pathinfo("./poc.php", PATHINFO_DIRNAME);
+
+
+// ==================================================================================================================
+
+
+class Collection implements IteratorAggregate
+{
+    public function getIterator()
+    {
+        return new ArrayIterator([]);
+    }
+}
+
+class ExtendedCollection extends Collection
+{
+    /**
+     * @return \ArrayIterator<int, string>
+     */
+    public function getIterator(): \ArrayIterator
+    {
+        return new \ArrayIterator(['hello']);
+    }
+}
+
+// ==================================================================================================================
+
+/**@template X */
+class Foo {}
+
+/** @return \Generator<int, Foo<string>, mixed, void> */
+function generate()
+{
+    /** @var Foo<string> $x */
+    $x = new Foo();
+    yield $x;
+}
